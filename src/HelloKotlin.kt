0ff3b1ee01816adc0049423e970b2ecfbd6a8fc0
Partha <pbiswas_hal@yahoo.co.in>
2012-07-16 19:30:30 +0530

    Fooling with Kotlin

---
+++ src/HelloKotlin.kt
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: pabiswas
+ * Date: 16/7/12
+ * Time: 7:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+package demo
+
+fun main(args: Array<String>) {
+    println("Hello Kotlin !!")
+}
